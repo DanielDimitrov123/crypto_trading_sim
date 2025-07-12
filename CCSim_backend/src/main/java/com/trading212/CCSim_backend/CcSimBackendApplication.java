8bddf930eb37170e5778d859529a78eb5aa29c03
dani <danitobg111@gmail.com>
2025-07-12 10:29:22 +0300

    adding java spring backend

---
+++ CCSim_backend/src/main/java/com/trading212/CCSim_backend/CcSimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trading212.CCSim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcSimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcSimBackendApplication.class, args);
+	}
+
+}
---
+++ CCSim_backend/src/test/java/com/trading212/CCSim_backend/CcSimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading212.CCSim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcSimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
