f9f98e39458ed70b85850d10bf6c7d258cdcc572
dani <danitobg111@gmail.com>
2025-07-14 08:02:34 +0300

    updating schema.sql

--- CCSim_backend/src/main/resources/schema.sql
+++ CCSim_backend/src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE table CryptoCurrency(){
+CREATE table Currency(){
     id int primary key AUTO_INCREMENT,
     eventName varchar[30]
 }
 
-CREATE table CryptoCurrencyPrices(){
+CREATE table CurrencyPrices(){
     id int primary key AUTO_INCREMENT,
     pricingDate DATE,
     currencyPrice INT,
-    CryptoCurrencyID INT,
-    FOREIGN KEY (CryptoCurrencyID) REFERENCES CryptoCurrency(id)
+    CurrencyID INT,
+    FOREIGN KEY (CurrencyID) REFERENCES Currency(id)
 }
 
 CREATE table Actions(){
@@ -16,15 +16,23 @@ CREATE table Actions(){
     date DATE,
     UsersID INT,
     ActionTypeID INT,
-    CryptoCurrencyPricesID INT,
+    CurrencyPricesID INT,
     FOREIGN KEY (UsersID) REFERENCES Users(id),
     FOREIGN KEY (ActionTypeID) REFERENCES ActionType(id),
-    FOREIGN KEY (CryptoCurrencyPricesID) REFERENCES CryptoCurrencyPrices(id)
+    FOREIGN KEY (CurrencyPricesID) REFERENCES CurrencyPrices(id)
 }
 
 Create table ActionType(){
     id int primary key AUTO_INCREMENT,
     typename varchar[20]
+}
+
+Create table Balance(){
+    id int primary key AUTO_INCREMENT,
+    UsersID INT,
+    CurrencyID INT,
+    FOREIGN KEY (UsersID) REFERENCES Users(id),
+    FOREIGN KEY (CurrencyID) REFERENCES Currency(id)
 }
 
 Create table Users(){
